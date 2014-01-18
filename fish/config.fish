0336c878c6df585e245f5153c396bdf75bf62c3c
Chris Thomson <chris@cthomson.ca>
2014-01-18 01:54:20 -0500

    Add aliases to fish.

--- fish/config.fish
+++ fish/config.fish
@@ -1,2 +1,12 @@
 bind \e\[1\;5C forward-word
 bind \e\[1\;5D backward-word
+
+# aliases
+alias ..='cd ..'
+alias b='bundle'
+alias bs='bundle exec rails server'
+alias bake='bundle exec rake'
+alias gs='git status'
+alias gst='git stash'
+alias gsa='git stash apply'
+alias gd='git diff'
