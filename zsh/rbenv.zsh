5b6b7cb1bbd4790e26a1d6b7c1685b522cd766e1
Chris Thomson <chris@cthomson.ca>
2014-01-30 17:06:36 -0500

    Add rbenv to zshrc.

---
+++ zsh/rbenv.zsh
@@ -0,0 +1,3 @@
+eval "$(rbenv init -)"
+export PATH="~/.rbenv/shims:$PATH"                                                                                                                                                                      
+source ~/.rbenv/completions/rbenv.zsh
