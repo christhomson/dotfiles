d5beab9e7ddb18cac5f69475ebf0e0f16e7b4ba8
Chris Thomson <chris@cthomson.ca>
2014-01-30 21:32:04 -0500

    Make ~/bin $PATH absolute.

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -3,7 +3,7 @@ export PATH="/sbin:$PATH"
 export PATH="/usr/bin:$PATH"
 export PATH="/usr/sbin:$PATH"
 export PATH="/usr/local/bin:$PATH"
-export PATH="~/bin:$PATH"
+export PATH="$HOME/bin:$PATH"
 
 if [[ LOCATION == "home" ]]; then
   export GOPATH="$HOME/dev/go"
