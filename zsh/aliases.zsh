693d745e794755dab3618bc0bd240b07f30a9465
Chris Thomson <chris@cthomson.ca>
2014-03-30 14:21:26 -0400

    Add some aliases.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,12 +2,16 @@ alias ..='cd ..'
 
 alias b='bundle'
 alias be='bundle exec'
+alias bx='bundle exec'
 alias bc='bundle exec rails console'
 alias bs='bundle exec rails server'
 alias bi='bundle install'
 alias bo='bundle open'
+alias bu='bundle update'
 alias bake='bundle exec rake'
+alias bt='bundle exec rake test'
 alias bails='bundle exec rails'
+alias ss='script/server'
 
 alias gs='git st'
 alias gst='git st'
@@ -15,8 +19,12 @@ alias gd='git diff'
 alias gbrt='git branch-timeline'
 alias gap='git add -p'
 alias grc='git rebase --continue'
+alias grh='git reset --hard'
 
 alias vi='vim'
+alias v='vi .'
+
+alias nm='nodemon'
 
 # Pretty print JSON.
 alias ppj='python -mjson.tool'
