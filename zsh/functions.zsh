9fe7a43619675aa79d36f74f19554d475084e630
Chris Thomson <chris@cthomson.ca>
2014-04-05 00:08:00 -0400

    Override rake and rails to automatically use Zeus or Bundler.

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -6,3 +6,25 @@ common_commands() {
   sort -rn |
   head -50
 }
+
+# Override rake and rails methods to automatically use `zeus` or `bundle exec`.
+rake() {
+  if [ -S '.zeus.sock' ]; then
+    zeus rake
+  elif [ -f 'Gemfile' ]; then
+    bundle exec rake
+  else
+    rake
+  fi
+}
+
+rails() {
+  if [[ -S '.zeus.sock' && ($1 = 's' || $1 = 'server' || $1 = 'g' || $1 = 'generate' || $1 = 'd' || $1 = 'destroy' || $1 = 'c' || $1 = 'console' || $1 = 'r' || $1 = 'runner') ]]; then
+    zeus $zeus_cmd ${*:2}
+  elif [ -f 'Gemfile' ]; then
+    bundle exec rails $1 ${*:2}
+  else
+    echo "I don't know what to do. You aren't using Zeus or Bundler."
+    return 1
+  fi
+}
