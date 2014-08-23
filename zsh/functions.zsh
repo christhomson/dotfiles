e285165d3f4f0755fec7819fb4fcb2d7772d86ae
Chris Thomson <chris@cthomson.ca>
2014-08-23 17:39:11 -0400

    Remove rake() and rails() override functions.

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -6,25 +6,3 @@ common_commands() {
   sort -rn |
   head -50
 }
-
-# Override rake and rails methods to automatically use `zeus` or `bundle exec`.
-rake() {
-  if [ -S '.zeus.sock' ]; then
-    zeus rake ${*:1}
-  elif [ -f 'Gemfile' ]; then
-    bundle exec rake ${*:1}
-  else
-    rake ${*:1}
-  fi
-}
-
-rails() {
-  if [[ -S '.zeus.sock' && ($1 = 's' || $1 = 'server' || $1 = 'g' || $1 = 'generate' || $1 = 'd' || $1 = 'destroy' || $1 = 'c' || $1 = 'console' || $1 = 'r' || $1 = 'runner') ]]; then
-    zeus $1 ${*:2}
-  elif [ -f 'Gemfile' ]; then
-    bundle exec rails $1 ${*:2}
-  else
-    echo "I don't know what to do. You aren't using Zeus or Bundler."
-    return 1
-  fi
-}
