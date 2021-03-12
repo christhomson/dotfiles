f65f665dc639d85df442e3a7af28bf43986bad8d
Chris Thomson <chris.thomson@shopify.com>
2021-03-12 15:42:20 -0500

    Only set git username in spin environment

--- setup.sh
+++ setup.sh
@@ -5,8 +5,10 @@ ln -sf ~/dotfiles/ruby/irbrc ~/.irbrc
 ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
 ln -sf ~/dotfiles/git/config ~/.gitconfig
 
-git config --global user.name "Chris Thomson"
-git config --global user.email "chris.thomson@shopify.com"
+if [ $SPIN ]; then
+  git config --global user.name "Chris Thomson"
+  git config --global user.email "chris.thomson@shopify.com"
+fi
 
 vim -E -s -u "$HOME/.vimrc" +PlugInstall +qall
 
