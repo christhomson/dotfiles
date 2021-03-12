b88b8477cbb089720a45600375bb8c550f883036
Chris Thomson <chris.thomson@shopify.com>
2021-03-12 14:55:18 -0500

    Symlink git config in setup script, but still setup git user info

--- setup.sh
+++ setup.sh
@@ -3,6 +3,10 @@
 ln -sf ~/dotfiles/vim/vimrc ~/.vimrc
 ln -sf ~/dotfiles/ruby/irbrc ~/.irbrc
 ln -sf ~/dotfiles/zsh/zshrc ~/.zshrc
+ln -sf ~/dotfiles/git/config ~/.gitconfig
+
+git config --global user.name "Chris Thomson"
+git config --global user.email "chris.thomson@shopify.com"
 
 vim -E -s -u "$HOME/.vimrc" +PlugInstall +qall
 
