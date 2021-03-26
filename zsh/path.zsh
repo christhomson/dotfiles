6b86e9320d7d4b481dcf95ac84a22b08b2e88fe4
Chris Thomson <chris.thomson@shopify.com>
2021-03-26 12:07:53 -0400

    Remove chruby and tweak PATHs I need
    The PATHs were conflicting with something in the Spin environment.

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,16 +1,6 @@
-export GOPATH="$HOME/dev/go"
-
-export PATH="/bin:$PATH"
 export PATH="/sbin:$PATH"
-export PATH="/usr/bin:$PATH"
 export PATH="/usr/sbin:$PATH"
 export PATH="/usr/local/bin:$PATH"
 export PATH="$HOME/bin:$PATH"
-export PATH="$HOME/.config/bin:$PATH"
-export PATH="$HOME/.config/bin/icdiff:$PATH"
-
-if [[ LOCATION == "home" ]]; then
-  export PATH="/usr/texbin:$PATH"
-  export PATH="/Users/chris/Dropbox/Development/MarmosetSubmit/bin:$PATH"
-  export PATH="$GOPATH/bin:$PATH"
-fi
+export PATH="$HOME/dotfiles/bin:$PATH"
+export PATH="$HOME/dotfiles/bin/icdiff:$PATH"
